6373c158a1e005d0b30ea0d116f6b3d5daa6b9fc
Artemiy Kuzin <kuzin_artemiy@list.ru>
2022-10-08 02:05:17 +0500

    feat: change userMenu styles

--- client/src/utils/mockData/userMenuMockData.ts
+++ client/src/utils/mockData/userMenuMockData.ts
@@ -14,100 +14,39 @@ export type TUserMenuMockDataType = {
 export const userMenuMockData: TUserMenuMockDataType[] = [
   {
     id: '1',
-    title: 'Профиль сотрудника',
-    link: '/',
-    innerMenu: [
-      {
-        id: '1',
-        title: 'test1',
-        link: '/',
-      },
-      {
-        id: '2',
-        title: 'test2',
-        link: '/',
-      },
-      {
-        id: '3',
-        title: 'test3',
-        link: '/',
-      },
-    ],
+    title: 'Профиль пилота',
+    link: '/profile',
   },
   {
     id: '2',
-    title: 'Лига ВТБ',
-    link: '/league',
+    title: 'Формула первых',
+    link: '/formula',
     innerMenu: [
       {
         id: '1',
-        title: 'test1',
+        title: 'Мои ресурсы',
         link: '/',
       },
       {
         id: '2',
-        title: 'test2',
+        title: 'VTB Store',
         link: '/',
       },
       {
         id: '3',
-        title: 'test3',
+        title: 'Правила игры',
         link: '/',
       },
     ],
   },
   {
     id: '3',
-    title: 'Мероприятия и курсы',
-    link: '/events',
-    innerMenu: [
-      {
-        id: '1',
-        title: 'test1',
-        link: '/',
-      },
-      {
-        id: '2',
-        title: 'test2',
-        link: '/',
-      },
-      {
-        id: '3',
-        title: 'test3',
-        link: '/',
-      },
-    ],
+    title: 'Рейтинг первых',
+    link: '/ratings',
   },
   {
     id: '4',
-    title: 'Телефонный справочник',
-    link: '/phonebook',
-  },
-  {
-    id: '5',
-    title: 'База знаний',
-    link: '/knowledge',
-  },
-  {
-    id: '6',
-    title: 'ВТБ Store',
-    link: '/store',
-    innerMenu: [
-      {
-        id: '1',
-        title: 'test1',
-        link: '/',
-      },
-      {
-        id: '2',
-        title: 'test2',
-        link: '/',
-      },
-      {
-        id: '3',
-        title: 'test3',
-        link: '/',
-      },
-    ],
+    title: 'Список пилотов',
+    link: '/pilots',
   },
 ];
