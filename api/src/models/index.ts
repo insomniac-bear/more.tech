4e589516c598ca2c08db6ec0bed60b5578590d86
Kirill <bear.wdev@gmail.com>
2022-10-07 22:53:34 +0300

    feat: add Skill model

--- api/src/models/index.ts
+++ api/src/models/index.ts
@@ -1,11 +1,13 @@
 import { Department } from './department';
 import { Role } from './role';
+import { Skill } from './skill';
 import { User } from './user';
 import { Wallet } from './wallet';
 
 export {
   Department,
   Role,
+  Skill,
   User,
   Wallet,
 };
