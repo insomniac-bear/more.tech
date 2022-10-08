4c56282aaefa9a01ca6f63460850ef5c8d2953fc
AndreiKolosov <AndreiKolosov@users.noreply.github.com>
2022-10-08 15:06:22 +0200

    feat: team preview styles

--- client/src/components/MainPagePreview/TeamPreview/TeamPreview.tsx
+++ client/src/components/MainPagePreview/TeamPreview/TeamPreview.tsx
@@ -4,7 +4,24 @@ import addLogo from '../../../images/ADD_logo.svg';
 
 const TeamPreview: FC = () => (
   <div className={styles.container}>
-    <img src={addLogo} alt="ADD" />
+    <div className={styles.logoWrapper}>
+      <img className={styles.logo} src={addLogo} alt="ADD" />
+    </div>
+    <div className={styles.listContainer}>
+      <p className={styles.listHeading}>Пилоты</p>
+      <ul className={styles.teamList}>
+        <li className={styles.listItem}>Шакурова Эвина</li>
+        <li className={styles.listItem}>Феоктистова Наталия</li>
+        <li className={styles.listItem}>Кузин Артемий</li>
+        <li className={styles.listItem}>Колосов Андрей</li>
+      </ul>
+    </div>
+    <div className={styles.listContainer}>
+      <p className={styles.listHeading}>Капитан</p>
+      <ul className={styles.teamList}>
+        <li className={styles.listItem}>Черепанов Кирилл</li>
+      </ul>
+    </div>
   </div>
 );
 
