57b1eaedd163446eadbd5a2ad8a3b0fe2f117fe2
AndreiKolosov <AndreiKolosov@users.noreply.github.com>
2022-10-08 16:47:10 +0200

    feat: add collapsible item

--- client/src/components/UserWallet/UserWallet.tsx
+++ client/src/components/UserWallet/UserWallet.tsx
@@ -1,10 +1,15 @@
 import { FC } from 'react';
+import SideMenuCollapsibleItem from '../SideMenuCollapsibleItem/SideMenuCollapsibleItem';
 import styles from './UserWallet.module.css';
 import { IUserWalletProps } from './UserWallet.props';
 
 const UserWallet: FC<IUserWalletProps> = ({ className = '', ...props }) => (
   <div className={`${styles.wallet} ${className}`} {...props}>
     <p className={styles.label}>Мои ресурсы</p>
+    <SideMenuCollapsibleItem label="Digital Ruble" />
+    <SideMenuCollapsibleItem label="Лимит «Спасибо»" />
+    <SideMenuCollapsibleItem label="История операций" />
+    <SideMenuCollapsibleItem label="Ресурсы команды" />
   </div>
 );
 
