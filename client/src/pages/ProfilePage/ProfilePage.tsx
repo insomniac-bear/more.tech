f0cabd6f2200366e1b8b944601ce8332b186d604
AndreiKolosov <AndreiKolosov@users.noreply.github.com>
2022-10-08 20:18:38 +0200

    feat: add secondary btn, fix styles in controll panel

--- client/src/pages/ProfilePage/ProfilePage.tsx
+++ client/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import Header from '../../components/Header/Header';
 import ProgressBar from '../../components/ProgressBar/ProgressBar';
+import SecondaryButton from '../../components/SecondaryButton/SecondaryButton';
 import UserCard from '../../components/UserCard/UserCard';
 import UserWallet from '../../components/UserWallet/UserWallet';
 import { user } from '../../utils/mockData/userMockData';
@@ -17,6 +18,7 @@ const ProfilePage: FC = () => (
       </div>
       <UserWallet className={styles.wallet} />
     </main>
+    <SecondaryButton type="button">Поблагодарить</SecondaryButton>
   </div>
 );
 
